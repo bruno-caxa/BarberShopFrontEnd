0ad24ef39d47f3aee0552e1541f2a68a6eadd8e9
bruno-caxa <frian6123@gmail.com>
2025-11-24 13:32:26 -0300

    correctly configuring the localePt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,15 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { routes } from './app.routes';
+
+registerLocaleData(localePt, 'pt');
 
 export const appConfig: ApplicationConfig = {
   providers: [
